3444d52cb9646cb50b6bdfb1928e0cf0024b473e
Lada-Rom <doodleztoon@gmail.com>
2021-12-11 19:59:32 +0300

    Loading and drawing texture

--- glut_test/glut_test.cpp
+++ glut_test/glut_test.cpp
@@ -1,83 +1,77 @@
-#include <iostream>
+﻿#if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <string>
 #include "glut.h"
-#include "jitter.h"
 
-float aquarium_width = 16.5;
-float aquarium_height = 14.5;
-float aquarium_depth = 2.5;
-
-const float aquarium_points[] = {
-    -0.5 * aquarium_width, -0.5 * aquarium_height, 0,
-     0.5 * aquarium_width, -0.5 * aquarium_height, 0,
-     0.5 * aquarium_width,  0.5 * aquarium_height, 0,
-    -0.5 * aquarium_width,  0.5 * aquarium_height, 0,
-    -0.5 * aquarium_width, -0.5 * aquarium_height, -aquarium_depth,
-     0.5 * aquarium_width, -0.5 * aquarium_height, -aquarium_depth,
-     0.5 * aquarium_width,  0.5 * aquarium_height, -aquarium_depth,
-    -0.5 * aquarium_width,  0.5 * aquarium_height, -aquarium_depth,
-};
+//Параметры текстуры
+GLuint texture;
+int w = 0, h = 0, n = 0;
+float angle = 0;
 
 void init() {
-    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glShadeModel(GL_FLAT);
+    glEnable(GL_DEPTH_TEST);
+
+    unsigned char *data = stbi_load(std::string("image.png").c_str(), &w, &h, &n, 3);
+    if (!data)
+        return;
+
+    //create a texture
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, 3, w, h, GL_RGB, GL_UNSIGNED_BYTE, data);
+    //texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    free(data);
 }
 
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_TEXTURE_2D);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
-    glLoadIdentity();
-    gluLookAt(0.0, 0.0, 9.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-
-    //glLoadIdentity();
+    angle += 0.02;
     glPushMatrix();
-    glBegin(GL_LINE_LOOP);
-        glColor3f(0.0, 0.0, 0.0);
-        glVertex3f(aquarium_points[0], aquarium_points[1], aquarium_points[2]);
-        glVertex3f(aquarium_points[3], aquarium_points[4], aquarium_points[5]);
-        glVertex3f(aquarium_points[6], aquarium_points[7], aquarium_points[8]);
-        glVertex3f(aquarium_points[9], aquarium_points[10], aquarium_points[11]);
+    glTranslatef(0.0, 0.0, -3.6);
+    glRotatef(angle, 0, 1, 0);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 0.0); glVertex3f(-2.0 * w / h,  2.0, 0.0);
+    glTexCoord2f(0.0, 1.0); glVertex3f(-2.0 * w / h, -2.0, 0.0);
+    glTexCoord2f(1.0, 1.0); glVertex3f( 2.0 * w / h, -2.0, 0.0);
+    glTexCoord2f(1.0, 0.0); glVertex3f( 2.0 * w / h,  2.0, 0.0);
     glEnd();
-
-    glBegin(GL_LINE_LOOP);
-        glVertex3f(aquarium_points[12], aquarium_points[13], aquarium_points[14]);
-        glVertex3f(aquarium_points[15], aquarium_points[16], aquarium_points[17]);
-        glVertex3f(aquarium_points[18], aquarium_points[19], aquarium_points[20]);
-        glVertex3f(aquarium_points[21], aquarium_points[22], aquarium_points[23]);
-    glEnd();
-
-    glBegin(GL_LINES);
-        glVertex3f(aquarium_points[0], aquarium_points[1], aquarium_points[2]);
-        glVertex3f(aquarium_points[12], aquarium_points[13], aquarium_points[14]);
-        glVertex3f(aquarium_points[3], aquarium_points[4], aquarium_points[5]);
-        glVertex3f(aquarium_points[15], aquarium_points[16], aquarium_points[17]);
-        glVertex3f(aquarium_points[6], aquarium_points[7], aquarium_points[8]);
-        glVertex3f(aquarium_points[18], aquarium_points[19], aquarium_points[20]);
-        glVertex3f(aquarium_points[9], aquarium_points[10], aquarium_points[11]);
-        glVertex3f(aquarium_points[21], aquarium_points[22], aquarium_points[23]);
-    glEnd();
-
     glPopMatrix();
-
-
-
     glutSwapBuffers();
     glutPostRedisplay();
-    //glFlush();
+    //glDisable(GL_TEXTURE_2D);
 }
 
 void reshape(int w, int h) {
     glViewport(0, 0, (GLsizei)w, (GLsizei)h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(90, (GLfloat)w / (GLfloat)h, 1, 50);
+    gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 1.0, 30.0);
     glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
-void main() {
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Scene");
+void main(int argc, char *argv) {
+    glutInit(&argc, &argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Texture");
     init();
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
