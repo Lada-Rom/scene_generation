01a4136ff62da0bcbbb8754a02d568100c8d5df9
Lada-Rom <doodleztoon@gmail.com>
2022-05-18 23:06:50 +0300

    [glut_test] nice reflection with stencil

--- glut_test/glut_test.cpp
+++ glut_test/glut_test.cpp
@@ -286,71 +286,298 @@
 //}
 
 
-int image_width_{ };
-int image_height_{ };
-int image_comp_{ };
+//int image_width_{ };
+//int image_height_{ };
+//int image_comp_{ };
+//
+//GLuint tex;
+//GLUquadric* sphere;
+//
+//void loadTexture(std::string filename) {
+//    unsigned char* data = stbi_load(filename.c_str(), &image_width_, &image_height_, &image_comp_, 3);
+//    if (!data)
+//        return;
+//
+//    glGenTextures(1, &tex);
+//    glBindTexture(GL_TEXTURE_2D, tex);
+//    gluBuild2DMipmaps(GL_TEXTURE_2D, 3, image_width_, image_height_, GL_RGB, GL_UNSIGNED_BYTE, data);
+//    
+//    //texture filtering
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+//    free(data);
+//}
+//
+//void draw(void) {
+//    glClearColor(0.5, 0.5, 1.0, 1);
+//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//
+//    glMatrixMode(GL_MODELVIEW);
+//    glLoadIdentity();
+//    glTranslatef(0.0, 0.0, -8.0);
+//    //glRotatef(45, 0, 1, 0);
+//
+//    gluQuadricDrawStyle(sphere, GLU_FILL);
+//    glBindTexture(GL_TEXTURE_2D, tex);
+//    gluQuadricTexture(sphere, GL_TRUE);
+//    gluQuadricNormals(sphere, GLU_SMOOTH);
+//    gluSphere(sphere, 1.0, 32, 32);
+//
+//    glutSwapBuffers();
+//}
+//
+//void resize(int w, int h) {
+//    if (!h)
+//        h = 1;
+//    glViewport(0, 0, w, h);
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
+//    gluPerspective(37, 1.0 * w / h, 0.1, 100.0);
+//}
+//
+//void init(void) {
+//    glEnable(GL_DEPTH_TEST);
+//    loadTexture("../../data/src/test_texture.png");
+//    sphere = gluNewQuadric();
+//    glEnable(GL_TEXTURE_2D);
+//}
+//
+//int main(int argc, char** argv) {
+//    glutInit(&argc, argv);
+//
+//    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+//    glutInitWindowSize(640, 512);
+//    glutCreateWindow("Test");
+//    glutDisplayFunc(draw);
+//    glutReshapeFunc(resize);
+//    init();
+//    glutMainLoop();
+//}
 
-GLuint tex;
-GLUquadric* sphere;
 
-void loadTexture(std::string filename) {
-    unsigned char* data = stbi_load(filename.c_str(), &image_width_, &image_height_, &image_comp_, 3);
-    if (!data)
-        return;
+const float LIGHT_POS = 7.0f; //The length of each side of the cube
+const float BOX_HEIGHT = LIGHT_POS; //The height of the box off of the ground
+const float FLOOR_SIZE = 20.0f; //The length of each side of the floor
 
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    gluBuild2DMipmaps(GL_TEXTURE_2D, 3, image_width_, image_height_, GL_RGB, GL_UNSIGNED_BYTE, data);
-    
-    //texture filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-    free(data);
+//Draws the cube
+void drawCube(float angle) {
+
+	glPushMatrix();
+	glRotatef(-angle, 1.0f, 1.0f, 0.0f);
+
+	glBegin(GL_QUADS);
+	//Cross back
+	glColor4f(0.0f, 1.0f, 0.0f, 0.9f);
+	glVertex3f(-0.5, 2.5, -0.5);
+	glVertex3f(-0.5, -3.0, -0.5);
+	glVertex3f(0.5, -3.0, -0.5);
+	glVertex3f(0.5, 2.5, -0.5);
+
+	glVertex3f(-2.0, 1.0, -0.5);
+	glVertex3f(-2.0, 0.0, -0.5);
+	glVertex3f(2.0, 0.0, -0.5);
+	glVertex3f(2.0, 1.0, -0.5);
+
+
+	//Cross front
+	glColor4f(1.0f, 1.0f, 0.0f, 0.9f);
+	glVertex3f(-0.5, 2.5, 0.5);
+	glVertex3f(-0.5, -3.0, 0.5);
+	glVertex3f(0.5, -3.0, 0.5);
+	glVertex3f(0.5, 2.5, 0.5);
+
+	glVertex3f(-2.0, 1.0, 0.5);
+	glVertex3f(-2.0, 0.0, 0.5);
+	glVertex3f(2.0, 0.0, 0.5);
+	glVertex3f(2.0, 1.0, 0.5);
+
+	//Cross top
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(0.5, 2.5, 0.5);
+	glVertex3f(-0.5, 2.5, 0.5);
+	glVertex3f(-0.5, 2.5, -0.5);
+	glVertex3f(0.5, 2.5, -0.5);
+	//Cross left top side
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(-0.5, 2.5, 0.5);
+	glVertex3f(-0.5, 2.5, -0.5);
+	glVertex3f(-0.5, 1.0, -0.5);
+	glVertex3f(-0.5, 1.0, 0.5);
+	//Cross left side  top
+
+	glVertex3f(-0.5, 1.0, 0.5);
+	glVertex3f(-0.5, 1.0, -0.5);
+	glVertex3f(-2.0, 1.0, -0.5);
+	glVertex3f(-2.0, 1.0, 0.5);
+
+	//Cross left side  end
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(-2.0, 1.0, 0.5);
+	glVertex3f(-2.0, 1.0, -0.5);
+	glVertex3f(-2.0, 0.0, -0.5);
+	glVertex3f(-2.0, 0.0, 0.5);
+
+	//Cross left side bottom
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(-2.0, 0.0, 0.5);
+	glVertex3f(-0.5, 0.0, 0.5);
+	glVertex3f(-0.5, 0.0, -0.5);
+	glVertex3f(-2.0, 0.0, -0.5);
+	//Cross left bottom side
+	glVertex3f(-0.5, 0.0, 0.5);
+	glVertex3f(-0.5, 0.0, -0.5);
+	glVertex3f(-0.5, -3.0, -0.5);
+	glVertex3f(-0.5, -3.0, 0.5);
+	//Cross  bottom
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(0.5, -3.0, 0.5);
+	glVertex3f(0.5, -3.0, -0.5);
+	glVertex3f(-0.5, -3.0, -0.5);
+	glVertex3f(-0.5, -3.0, 0.5);
+
+	//Cross right bottom side
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(0.5, 0.0, 0.5);
+	glVertex3f(0.5, -3.0, 0.5);
+	glVertex3f(0.5, -3.0, -0.5);
+	glVertex3f(0.5, 0.0, -0.5);
+
+	//Cross right side bottom
+	glVertex3f(0.5, 0.0, 0.5);
+	glVertex3f(0.5, 0.0, -0.5);
+	glVertex3f(2.0, 0.0, -0.5);
+	glVertex3f(2.0, 0.0, 0.5);
+	//Cross right side end
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(2.0, 1.0, 0.5);
+	glVertex3f(2.0, 0.0, 0.5);
+	glVertex3f(2.0, 0.0, -0.5);
+	glVertex3f(2.0, 1.0, -0.5);
+
+	//Cross right side top
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(0.5, 1.0, 0.5);
+	glVertex3f(2.0, 1.0, 0.5);
+	glVertex3f(2.0, 1.0, -0.5);
+	glVertex3f(0.5, 1.0, -0.5);
+	//Cross right side top
+	glVertex3f(0.5, 1.0, 0.5);
+	glVertex3f(0.5, 1.0, -0.5);
+	glVertex3f(0.5, 2.5, -0.5);
+	glVertex3f(0.5, 2.5, 0.5);
+	glEnd();
+	glPopMatrix();
 }
 
-void draw(void) {
-    glClearColor(0.5, 0.5, 1.0, 1);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glTranslatef(0.0, 0.0, -8.0);
-    //glRotatef(45, 0, 1, 0);
-
-    gluQuadricDrawStyle(sphere, GLU_FILL);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    gluQuadricTexture(sphere, GL_TRUE);
-    gluQuadricNormals(sphere, GLU_SMOOTH);
-    gluSphere(sphere, 1.0, 32, 32);
-
-    glutSwapBuffers();
+//Draws the floor
+void drawFloor() {
+	glBegin(GL_QUADS);
+	glNormal3f(0, 1, 0);
+	glVertex3f(-FLOOR_SIZE / 2, 0, FLOOR_SIZE / 2);
+	glVertex3f(-FLOOR_SIZE / 2, 0, -FLOOR_SIZE / 2);
+	glVertex3f(FLOOR_SIZE / 2, 0, -FLOOR_SIZE / 2);
+	glVertex3f(FLOOR_SIZE / 2, 0, FLOOR_SIZE / 2);
+	glEnd();
 }
 
-void resize(int w, int h) {
-    if (!h)
-        h = 1;
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(37, 1.0 * w / h, 0.1, 100.0);
+float _angle = 0;
+
+void initRendering() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_COLOR_MATERIAL);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
-void init(void) {
-    glEnable(GL_DEPTH_TEST);
-    loadTexture("../../data/src/test_texture.png");
-    sphere = gluNewQuadric();
-    glEnable(GL_TEXTURE_2D);
+void handleResize(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (float)w / (float)h, 1.0, 200.0);
+}
+
+void drawScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTranslatef(0.0f, 0.0f, -40.0f);
+	glRotatef(30, 1, 0, 0);
+
+	GLfloat ambientLight[] = { 0.3f, 0.3f, 0.3f, 1.0f };
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLight);
+
+	GLfloat lightColor[] = { 0.7f, 0.7f, 0.7f, 1.0f };
+	GLfloat lightPos[] = { -2 * LIGHT_POS , LIGHT_POS , 4 * LIGHT_POS , 1.0f };
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+
+	glPushMatrix();
+	glTranslatef(0, BOX_HEIGHT, 0);
+	drawCube(_angle);
+	glPopMatrix();
+
+	glEnable(GL_STENCIL_TEST); //Enable using the stencil buffer
+	glColorMask(0, 0, 0, 0); //Disable drawing colors to the screen
+	glDisable(GL_DEPTH_TEST); //Disable depth testing
+	glStencilFunc(GL_ALWAYS, 1, 1); //Make the stencil test always pass
+	//Make pixels in the stencil buffer be set to 1 when the stencil test passes
+	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+	//Set all of the pixels covered by the floor to be 1 in the stencil buffer
+	drawFloor();
+
+	glColorMask(1, 1, 1, 1); //Enable drawing colors to the screen
+	glEnable(GL_DEPTH_TEST); //Enable depth testing
+	//Make the stencil test pass only when the pixel is 1 in the stencil buffer
+	glStencilFunc(GL_EQUAL, 1, 1);
+	glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP); //Make the stencil buffer not change
+
+	//Draw the cube, reflected vertically, at all pixels where the stencil
+	//buffer is 1
+	glPushMatrix();
+	glScalef(1, -1, 1);
+	glTranslatef(0, BOX_HEIGHT, 0);
+	drawCube(_angle);
+	glPopMatrix();
+
+	glDisable(GL_STENCIL_TEST); //Disable using the stencil buffer
+
+	//Blend the floor onto the screen
+	glEnable(GL_BLEND);
+	glColor4f(0, 0, 0.5, 0.5f);
+	drawFloor();
+	glDisable(GL_BLEND);
+
+	glutSwapBuffers();
+}
+
+void update(int value) {
+	_angle += 1.0f;
+	if (_angle > 360) {
+		_angle -= 360;
+	}
+
+	glutPostRedisplay();
+	glutTimerFunc(25, update, 0);
 }
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_STENCIL);
+	glutInitWindowSize(600, 600);
 
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowSize(640, 512);
-    glutCreateWindow("Test");
-    glutDisplayFunc(draw);
-    glutReshapeFunc(resize);
-    init();
-    glutMainLoop();
+	glutCreateWindow("www.codeincodeblock.blogspot.com");
+	initRendering();
+
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(handleResize);
+	glutTimerFunc(25, update, 0);
+
+	glutMainLoop();
+	return 0;
 }
