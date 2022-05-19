cc811221a680041373d0e893bb097a8a3a1f99ff
Lada-Rom <doodleztoon@gmail.com>
2022-05-19 18:56:46 +0300

    [glut_test] reflection in plane (non-zero height)

--- glut_test/glut_test.cpp
+++ glut_test/glut_test.cpp
@@ -584,77 +584,131 @@
 //}
 
 
+//void initRendering() {
+//	glClearColor(1.0, 1.0, 1.0, 1.0);
+//	glEnable(GL_DEPTH_TEST);
+//	glEnable(GL_LIGHTING);
+//	glEnable(GL_LIGHT0);
+//	glEnable(GL_NORMALIZE);
+//}
+//
+//void drawScene() {
+//	glMatrixMode(GL_MODELVIEW);
+//	glLoadIdentity();
+//
+//	glTranslatef(0.0f, 0.0f, -20.0f);
+//
+//	GLfloat no_mat[] = { 0.0,0.0,0.0,1.0 };
+//	GLfloat mat_ambient[] = { 0.7,0.7,0.7,1.0 };
+//	GLfloat mat_ambient_color[] = { 0.8,0.8,0.2,1.0 };
+//	GLfloat mat_diffuse[] = { 0.1,0.5,0.8,1.0 };
+//	GLfloat mat_specular[] = { 1.0,1.0,1.0,1.0 };
+//	GLfloat no_shininess[] = { 0.0 };
+//	GLfloat low_shininess[] = { 5.0 };
+//	GLfloat high_shininess[] = { 100.0 };
+//	GLfloat mat_emission[] = { 0.3,0.2,0.2,0.0 };
+//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//
+//	//first left sphere
+//	glPushMatrix();
+//	glTranslatef(-3.75, 0.0, 0.0);
+//	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
+//	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+//	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+//	glMaterialfv(GL_FRONT, GL_SHININESS, no_shininess);
+//	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
+//	glutSolidSphere(1.0, 16, 16);
+//	glPopMatrix();
+//
+//	//second sphere
+//	glPushMatrix();
+//	glTranslatef(-1.25, 0.0, 0.0);
+//	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
+//	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+//	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+//	glMaterialfv(GL_FRONT, GL_SHININESS, low_shininess);
+//	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
+//	glutSolidSphere(1.0, 16, 16);
+//	glPopMatrix();
+//
+//	//third sphere
+//	glPushMatrix();
+//	glTranslatef(1.25, 0.0, 0.0);
+//	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
+//	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+//	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+//	glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+//	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
+//	glutSolidSphere(1.0, 16, 16);
+//	glPopMatrix();
+//
+//	//last right sphere
+//	glPushMatrix();
+//	glTranslatef(3.75, 0.0, 0.0);
+//	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
+//	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+//	glMaterialfv(GL_FRONT, GL_SPECULAR, no_mat);
+//	glMaterialfv(GL_FRONT, GL_SHININESS, no_shininess);
+//	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
+//	glutSolidSphere(1.0, 16, 16);
+//	glPopMatrix();
+//
+//	glutSwapBuffers();
+//	glutPostRedisplay();
+//}
+//
+//void handleResize(int w, int h) {
+//	glViewport(0, 0, w, h);
+//	glMatrixMode(GL_PROJECTION);
+//	glLoadIdentity();
+//	gluPerspective(37.0, (float)w / (float)h, 1.0, 200.0);
+//}
+//
+//int main(int argc, char** argv) {
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_STENCIL);
+//	glutInitWindowSize(600, 600);
+//	glutCreateWindow("My reflection");
+//	initRendering();
+//	glutDisplayFunc(drawScene);
+//	glutReshapeFunc(handleResize);
+//	glutMainLoop();
+//}
+
+const float LIGHT_POS = 7.0f; //The length of each side of the cube
+const float BOX_HEIGHT = 4; //The height of the box off of the ground
+const float FLOOR_SIZE = 20.0f; //The length of each side of the floor
+const float floor_on = -2;
+
+//Draws the cube
+void drawCube() {
+	glPushMatrix();
+	glutSolidSphere(3, 10, 10);
+	glPopMatrix();
+}
+
+//Draws the floor
+void drawFloor() {
+	glPushMatrix();
+	glTranslated(0, floor_on, 0);
+		glBegin(GL_QUADS);
+			//glNormal3f(0, 1, 0);
+			glVertex3f(-FLOOR_SIZE / 2, 0, FLOOR_SIZE / 2);
+			glVertex3f(-FLOOR_SIZE / 2, 0, -FLOOR_SIZE / 2);
+			glVertex3f(FLOOR_SIZE / 2, 0, -FLOOR_SIZE / 2);
+			glVertex3f(FLOOR_SIZE / 2, 0, FLOOR_SIZE / 2);
+		glEnd();
+	glPopMatrix();
+}
+
 void initRendering() {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_NORMALIZE);
-}
-
-void drawScene() {
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	glTranslatef(0.0f, 0.0f, -20.0f);
-
-	GLfloat no_mat[] = { 0.0,0.0,0.0,1.0 };
-	GLfloat mat_ambient[] = { 0.7,0.7,0.7,1.0 };
-	GLfloat mat_ambient_color[] = { 0.8,0.8,0.2,1.0 };
-	GLfloat mat_diffuse[] = { 0.1,0.5,0.8,1.0 };
-	GLfloat mat_specular[] = { 1.0,1.0,1.0,1.0 };
-	GLfloat no_shininess[] = { 0.0 };
-	GLfloat low_shininess[] = { 5.0 };
-	GLfloat high_shininess[] = { 100.0 };
-	GLfloat mat_emission[] = { 0.3,0.2,0.2,0.0 };
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	//first left sphere
-	glPushMatrix();
-	glTranslatef(-3.75, 0.0, 0.0);
-	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, no_shininess);
-	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
-	glutSolidSphere(1.0, 16, 16);
-	glPopMatrix();
-
-	//second sphere
-	glPushMatrix();
-	glTranslatef(-1.25, 0.0, 0.0);
-	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, low_shininess);
-	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
-	glutSolidSphere(1.0, 16, 16);
-	glPopMatrix();
-
-	//third sphere
-	glPushMatrix();
-	glTranslatef(1.25, 0.0, 0.0);
-	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
-	glutSolidSphere(1.0, 16, 16);
-	glPopMatrix();
-
-	//last right sphere
-	glPushMatrix();
-	glTranslatef(3.75, 0.0, 0.0);
-	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, no_mat);
-	glMaterialfv(GL_FRONT, GL_SHININESS, no_shininess);
-	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
-	glutSolidSphere(1.0, 16, 16);
-	glPopMatrix();
-
-	glutSwapBuffers();
-	glutPostRedisplay();
+	glEnable(GL_COLOR_MATERIAL);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void handleResize(int w, int h) {
@@ -664,13 +718,73 @@ void handleResize(int w, int h) {
 	gluPerspective(37.0, (float)w / (float)h, 1.0, 200.0);
 }
 
+void drawScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTranslatef(0.0f, 0.0f, -40.0f);
+	glRotatef(20, 1, 0, 0);
+
+	//GLfloat ambientLight[] = { 0.3f, 0.3f, 0.3f, 1.0f };
+	//glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLight);
+	//
+	//GLfloat lightColor[] = { 0.7f, 0.7f, 0.7f, 1.0f };
+	//GLfloat lightPos[] = { -2 * LIGHT_POS , LIGHT_POS , 4 * LIGHT_POS , 1.0f };
+	//glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
+	//glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+
+	glColor3f(0, 0, 0.5);
+	glPushMatrix();
+	glTranslatef(0, BOX_HEIGHT, 0);
+	drawCube();
+	glPopMatrix();
+
+	glEnable(GL_STENCIL_TEST); //Enable using the stencil buffer
+	glColorMask(0, 0, 0, 0); //Disable drawing colors to the screen
+	glDisable(GL_DEPTH_TEST); //Disable depth testing
+	glStencilFunc(GL_ALWAYS, 1, 1); //Make the stencil test always pass
+	//Make pixels in the stencil buffer be set to 1 when the stencil test passes
+	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+	//Set all of the pixels covered by the floor to be 1 in the stencil buffer
+	drawFloor();
+
+	glColorMask(1, 1, 1, 1); //Enable drawing colors to the screen
+	glEnable(GL_DEPTH_TEST); //Enable depth testing
+	//Make the stencil test pass only when the pixel is 1 in the stencil buffer
+	glStencilFunc(GL_EQUAL, 1, 1);
+	glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP); //Make the stencil buffer not change
+
+	//Draw the cube, reflected vertically, at all pixels where the stencil
+	//buffer is 1
+	glPushMatrix();
+	glScalef(1, -1, 1);
+	glTranslatef(0, -floor_on, 0);
+	glTranslatef(0, BOX_HEIGHT, 0);
+	drawCube();
+	glPopMatrix();
+
+	glDisable(GL_STENCIL_TEST); //Disable using the stencil buffer
+
+	//Blend the floor onto the screen
+	glEnable(GL_BLEND);
+	glColor4f(0.5, 0, 0, 0.5f);
+	drawFloor();
+	glDisable(GL_BLEND);
+
+	glutSwapBuffers();
+}
+
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_STENCIL);
 	glutInitWindowSize(600, 600);
-	glutCreateWindow("My reflection");
+
+	glutCreateWindow("Sphere reflection");
 	initRendering();
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(handleResize);
 	glutMainLoop();
+	return 0;
 }
